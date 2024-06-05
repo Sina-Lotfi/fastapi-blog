56a4d29ff5dbc6acb721477a670dd4d492bcd262
Sina-Lotfi <sinalotfi531@gmail.com>
2024-06-05 23:54:10 +0330

    add delete input

--- schema/_input.py
+++ schema/_input.py
@@ -4,6 +4,13 @@ from pydantic import BaseModel
 class RegisterInput(BaseModel):
     username: str
     password: str
+
+
 class UpdataUserUsernameInput(BaseModel):
     old_username: str
     new_username: str
+
+
+class UserDeleteProfileInput(BaseModel):
+    username: str
+    password: str
