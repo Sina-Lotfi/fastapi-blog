7c7eb244b9988dd8f3446c26539cd19a0c19cb9f
Sina-Lotfi <sinalotfi531@gmail.com>
2024-06-05 23:53:54 +0330

    add delete query

--- oprations/users.py
+++ oprations/users.py
@@ -22,18 +22,29 @@ class UserOprations:
         async with self.db_session as session:
             user_data = await session.scalar(query)
             if user_data is None:
-                raise ValidationErr('User non found')
+                raise ValidationErr("User non found")
             return user_data
 
-    async def update_username(self, old_username:str, new_username:str) -> User:
+    async def update_username(self, old_username: str, new_username: str) -> User:
         query = sa.select(User).where(User.username == old_username)
-        update_query = sa.update(User).where(User.username == old_username).values(username=new_username)
+        update_query = (
+            sa.update(User)
+            .where(User.username == old_username)
+            .values(username=new_username)
+        )
         async with self.db_session as session:
             user_data = await session.scalar(query)
             if user_data is None:
-                raise ValidationErr('User non found')
+                raise ValidationErr("User non found")
             await session.execute(update_query)
             await session.commit()
             user_data.username = new_username
             return user_data
 
+    async def user_delete_account(self, username: str, password: str) -> None:
+        delete_query = sa.delete(User).where(
+            User.username == username, User.password == password
+        )
+        async with self.db_session as session:
+            await session.execute(delete_query)
+            await session.commit()
