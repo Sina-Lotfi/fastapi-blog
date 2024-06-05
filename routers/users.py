e653b1b8a535b4004554bb72d7e5d9f3194056da
Sina-Lotfi <sinalotfi531@gmail.com>
2024-06-05 23:54:03 +0330

    add delete router

--- routers/users.py
+++ routers/users.py
@@ -1,29 +1,23 @@
 from typing import Annotated
-from xml.dom import UserDataHandler
 
 from fastapi import APIRouter, Body, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.engins import get_db
 from oprations.users import UserOprations
-from schema._input import RegisterInput, UpdataUserUsernameInput
+from schema._input import RegisterInput, UpdataUserUsernameInput, UserDeleteProfileInput
 
 router = APIRouter()
 
 
 @router.post("/register")
 async def register(
-    db_session: Annotated[AsyncSession, Depends(get_db)],
-    data: RegisterInput = Body()
+    db_session: Annotated[AsyncSession, Depends(get_db)], data: RegisterInput = Body()
 ):
     user = await UserOprations(db_session).create(
         username=data.username, password=data.password
     )
     return user
-
-
-@router.post("/login")
-async def login(): ...
 
 
 @router.get("/{username}/")
@@ -37,7 +31,19 @@ async def get_user_profile(
 @router.put("/")
 async def user_update_profile(
     db_session: Annotated[AsyncSession, Depends(get_db)],
-    data: UpdataUserUsernameInput = Body()
+    data: UpdataUserUsernameInput = Body(),
 ):
-    user = await UserOprations(db_session).update_username(data.old_username, data.new_username)
+    user = await UserOprations(db_session).update_username(
+        data.old_username, data.new_username
+    )
     return user
+
+
+@router.delete("/")
+async def delete_user_profile(
+    db_session: Annotated[AsyncSession, Depends(get_db)],
+    data: UserDeleteProfileInput = Body(),
+):
+    await UserOprations(db_session).user_delete_account(
+        username=data.username, password=data.password
+    )
